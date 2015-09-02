061bbc2e175c7468902fb363025da95e702c98bf
inghamn <inghamn@bloomington.in.gov>
2015-09-02 09:49:10 -0400

    Added a build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+APPNAME=ivy
+DIR=`pwd`
+BUILD=$DIR/build
+
+if [ ! -d $BUILD ]
+	then mkdir $BUILD
+fi
+
+# The PHP code does not need to actually build anything.
+# Just copy all the files into the build
+rsync -rlv --exclude-from=$DIR/buildignore --delete $DIR/ $BUILD/$APPNAME
+cd $BUILD
+tar czvf $APPNAME.tar.gz $APPNAME
