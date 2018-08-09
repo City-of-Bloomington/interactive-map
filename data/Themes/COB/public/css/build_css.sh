bee7ae54cc73df428027241b5ffbd6c2ca8fb999
inghamn <inghamn@bloomington.in.gov>
2018-08-09 09:48:13 -0400

    Updated themes for latest FN1 release

--- data/Themes/COB/public/css/build_css.sh
+++ data/Themes/COB/public/css/build_css.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#node-sass --output-style compact --source-map ./ screen.scss ./screen.css
-sass --style compact screen.scss screen.css
+echo "Compiling COB theme CSS"
+pysassc -t compact -m screen.scss screen.css
--- data/Themes/Kirkwood/public/css/build_css.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Compiling theme CSS"
-node-sass --output-style compact --source-map ./ screen.scss ./screen.css
